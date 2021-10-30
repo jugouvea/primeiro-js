4644072aaaa818c6d1c1f93a12d1799ab628c9d8
Júlia Gouvêa <91096056+jugouvea@users.noreply.github.com>
2021-10-30 15:13:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const ano_atual = 2021
+let nome  = prompt("Qual o seu nome meu querido?")
+let ano_de_nascimento = prompt("Em que belo ano você nasceu?")
+let idade = ano_atual - ano_de_nascimento
+let mensagem = `Olá ${nome}, seja bem vindo! Você tem ${idade} anos, YAY!`
+
+alert (mensagem)
+
+
